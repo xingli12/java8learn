2e731374178aacb9b0d5043ba6047af70b8f991e
xingli12 <xingli12@iflytek.com>
2018-09-21 17:35:51 +0800

    算法4学习

---
+++ src/main/java/com/lx/App.java
@@ -0,0 +1,19 @@
+package com.lx;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        StdOut.println("haha");
+        StdOut.printf("%6s\n","haha");
+        System.out.printf("%6s\n","haha");
+    }
+
+}
